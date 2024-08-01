1fda3f66a8477e92ae52febf6a57540730a11983
Farhan Jamil <71662961+FarhanJ2@users.noreply.github.com>
2024-07-31 20:19:20 -0400

    Fix Misspell

--- src/main/java/frc/robot/utils/SwerveModuleConstants.kt
+++ src/main/java/frc/robot/utils/SwerveModuleConstants.kt
@@ -7,6 +7,6 @@ class SwerveModuleConstants
  * Swerve Module Constants to be used when creating swerve modules.
  * @param driveMotorID
  * @param angleMotorID
- * @param canCoderID
+ * @param cancoderID
  * @param angleOffset
  */(val driveMotorID: Int, val angleMotorID: Int, val cancoderID: Int, val angleOffset: Rotation2d)
